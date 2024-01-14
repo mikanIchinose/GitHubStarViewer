ce327b020a7a6d22e28d792671a0525f906a5ec9
Ichinose Yoshihiro <maruisansmai@gmail.com>
2024-01-15 00:01:16 +0900

    fixup! feat(data): APIクライアントをデータソースとして独立化

---
+++ feature/repositories/data/api/src/main/java/com/github/mikan/githubstarviewer/feature/repositories/data/api/model/StarredRepository.kt
@@ -0,0 +1,38 @@
+package com.github.mikan.githubstarviewer.feature.repositories.data.api.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class StarredRepository(
+    val id: Int,
+    val description: String?,
+    val name: String,
+    @SerialName("full_name")
+    val fullName: String,
+    val owner: Owner,
+    @SerialName("stargazers_count")
+    val stars: Int,
+    @SerialName("html_url")
+    val url: String,
+    val homepage: String?,
+    val language: String?,
+    @SerialName("languages_url")
+    val languagesUrl: String,
+    val license: License?,
+    val topics: List<String>,
+)
+
+@Serializable
+data class License(
+    val name: String,
+)
+
+@Serializable
+data class Owner(
+    val id: Int,
+    @SerialName("avatar_url")
+    val avatarUrl: String,
+    @SerialName("login")
+    val loginName: String,
+)
