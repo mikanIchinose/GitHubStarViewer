de785e7cf48f73ce77f6e9cd505cc62a3c19380c
Ichinose Yoshihiro <maruisansmai@gmail.com>
2024-01-14 23:59:59 +0900

    feat(data): APIクライアントをデータソースとして独立化

---
+++ feature/repositories/data/impl/src/main/java/com/github/mikan/githubstarviewer/feature/repositories/data/network/GitHubApi.kt
@@ -0,0 +1,25 @@
+package com.github.mikan.githubstarviewer.feature.repositories.data.network
+
+import com.github.mikan.githubstarviewer.feature.repositories.data.api.model.StarredRepository
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.serialization.json.JsonObject
+import javax.inject.Inject
+
+internal class GitHubApi @Inject constructor(
+    private val client: HttpClient,
+) {
+    fun getStarredRepositories(userName: String): Flow<List<StarredRepository>> = flow {
+        val response = client.get("users/$userName/starred")
+        emit(response.body<List<StarredRepository>>())
+    }
+
+    fun getLanguages(repository: StarredRepository): Flow<List<String>> = flow {
+        val response = client.get(repository.languagesUrl)
+        val jsonObject = response.body<JsonObject>()
+        emit(jsonObject.keys.toList())
+    }
+}
